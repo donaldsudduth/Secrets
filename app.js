8ff64859c94b0e103a918ddd08f4d3ae26f502b1
Donald Sudduth <donaldsudduth@gmail.com>
2020-09-11 20:24:17 -0400

    Add  10 rounds of salting to hash password

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 //jshint esversion:6
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const md5 = require("md5");
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 //connect to MongoDBAtlas
 mongoose.connect(
-  "mongodb+srv://" + process.env.DB_USER + ":"+ process.env.DB_PASSWORD +"@" + process.env.DB_TABLE ,
+  "mongodb+srv://" +
+    process.env.DB_USER +
+    ":" +
+    process.env.DB_PASSWORD +
+    "@" +
+    process.env.DB_TABLE,
   {
     useNewUrlParser: true,
   }
 );
 
-
 // mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true} );
 //create user schema
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
 });
 
 //encrypting user password through mongoose-encryption
@@ -31,7 +36,6 @@ const userSchema = new mongoose.Schema ({
 //create user model
 
 const User = new mongoose.model("User", userSchema);
-
 
 const app = express();
 
@@ -48,24 +52,26 @@ app.get("/login", function (req, res) {
   res.render("login");
 });
 
-
 //Log in users with POST request
 
-app.post("/login", function(req,res){
+app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser){
+  User.findOne({ email: username }, function (err, foundUser) {
     if (err) {
       console.log(err);
-    }else {
+    } else {
       if (foundUser) {
-        if (foundUser.password === password){
+         bcrypt.compare(password, foundUser.password, function(err,result){
+         if (result === true){
           res.render("secrets");
-        }
+        
       }
+    });
     }
-  });
+  }
+});
 });
 
 app.get("/register", function (req, res) {
@@ -74,24 +80,22 @@ app.get("/register", function (req, res) {
 
 //registering new users with a post request
 
-app.post("/register", function(req, res){
-
-  const newUser = new User ({
-    email: req.body.username,
-    password: md5(req.body.password)
+app.post("/register", function (req, res) {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
-newUser.save(function(err){
-  if(err){
-    console.log(err);
-  }else {res.render("secrets");
-}
 });
-});
-
-
 
 app.listen(3000 || process.end.PORT, function () {
   console.log("Server started successfully");
 });
-
-
